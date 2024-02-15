dcf93cb8b143e51c38792e342b782b77cfc4e9c2
JSanchez <jaypersanchez@gmail.com>
2024-02-16 06:08:02 +0800

    adding several linear regression

--- solana_analytics/closing_price_prediction.py
+++ solana_analytics/closing_price_prediction.py
@@ -1,3 +1,5 @@
+# Linear Regression with one independent variable 'open_price'
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
